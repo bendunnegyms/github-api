a4cd111fa282d0ed5eaefd48a9b9d07f208dee56
bendunnegyms <syrtsovv@tcd.ie>
2020-12-05 19:37:18 +0000

    fully working commit dag

--- vis/app.py
+++ vis/app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+import os.path
+from flask import Flask, render_template, send_from_directory, send_file
+import src.query as query
 
 app = Flask(__name__)
+assets_folder = os.path.join(app.root_path, 'data')
+print(assets_folder)
 
 @app.route("/")
 def index():
     return render_template("main.html")
+
+@app.route("/data/<path:filename>/")
+def data(filename):
+    query.dag_data()
+    try:
+        return send_from_directory(assets_folder, filename)
+    except FileNotFoundError:
+        abort(404)
--- vis/src/query.py
+++ vis/src/query.py
@@ -27,24 +27,39 @@ def get_repo_commits(r):
     # print(json.dumps(json.loads(commits_r.text), indent = 4))
     return json.loads(commits_r.text)
 
+def format_date(date):
+    f = date.split("T")
+    time = f[1].split("Z")[0]
+    res = "{date} - {time}".format(date = f[0], time = time)
+    return res
+
 def dagify_commits(r):
     # get latest commit, add as target with parent as source
     network_graph = {"nodes": [], "links": []}
     sha_data = []
     for commit in r:
         sha_data.append(commit.get("sha"))
-    print(str(sha_data))
+        
     nodes = []
     links = []
+    first = True
     for commit in r:
         # build node
-        commit_as_dict = {"id": "", "author": "", "date": "", "message": ""}
+        commit_as_dict = {"id": "", "date": "", "metadata": ""}
         c_sha = commit.get("sha")
         commit_as_dict["id"] = c_sha
         c_data = commit.get("commit")
-        commit_as_dict["author"] = c_data.get("author").get("name")
-        commit_as_dict["date"] = c_data.get("author").get("date")
-        commit_as_dict["message"] = c_data.get("message")
+        c_author = c_data.get("author").get("name")
+        c_date = c_data.get("author").get("date")
+        c_date = format_date(c_date)
+        c_message = c_data.get("message")
+        day = c_date.split(" ")[0]
+        commit_as_dict["date"] = day
+        metadata = "Message: {message} \nDate: {c_date} \nAuthor: {c_author}".format(message = c_message, c_date = c_date, c_author = c_author)
+        if first:
+            metadata += "\nLATEST COMMIT"
+            first = False
+        commit_as_dict["metadata"] = metadata
         nodes.append(commit_as_dict)
 
         #add links
@@ -57,7 +72,14 @@ def dagify_commits(r):
         
     network_graph["nodes"] = nodes
     network_graph["links"] = links
-    print(json.dumps(network_graph, indent= 2))
+    # print(json.dumps(network_graph, indent= 2))
+    with open("../vis/data/network_graph.json", "w") as fp:
+        json.dump(network_graph, fp)
+
+def dag_data():
+    r = json.loads(query("bendunnegyms").text)
+    repo_data = get_repo_by_name("SWENG-2", r)
+    dagify_commits(get_repo_commits(repo_data))
 
 
 if __name__ == "__main__":
